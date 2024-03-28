7a3ed3b18f2cfe7cda4ff43fa5e14639e1e5c8e3
mcpe500 <ivan04.android@gmail.com>
2024-03-28 10:46:32 +0700

    added swagger support

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,42 @@
-import express from 'express';
-import { ENV } from './config/environment';
-import router from './router/router';
-import cookieParser from 'cookie-parser';
-import connectDB from './connection/connection';
+import express, { Request, Response } from "express";
+import { ENV } from "./config/environment";
+import router from "./router/router";
+import cookieParser from "cookie-parser";
+import connectDB from "./connection/connection";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./router/swagger.json";
 
 const app = express();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-connectDB()
+connectDB();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Test api runs
+ *     description: Returns a Hello World message
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ */
+app.get("/", (req: Request, res: Response) => {
+    // res.cookie("jwt", "", {
+    //     httpOnly: true,
+    //     maxAge: 24*60*60*1000 // 1 hari
+    // })
+    return res.status(200).send("Hello World");
+});
+
 app.use("/api/v1", router);
 
-app.listen(ENV.PORT, () => console.log("Server is running at http://localhost:" + ENV.PORT));
+app.listen(ENV.PORT, () =>
+    console.log("Server is running at http://localhost:" + ENV.PORT)
+);
